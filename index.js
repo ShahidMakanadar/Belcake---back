538c146fdb17ec5a275045f1b651958b46a9cce6
Shahidm@2000 <shahidmakandar2000@gmail.com>
2023-10-24 00:39:19 +0530

    cors modify 3

--- index.js
+++ index.js
@@ -21,10 +21,10 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}))
 
 app.use(cors({
-    origin: 'https://belcake-shahidmakanadar.vercel.app', // Allow requests from this origin
+    origin: '*', // Allow requests from any origin
     methods: "GET, POST, PUT, DELETE",
     allowedHeaders: "Origin, X-Requested-With, Content-Type, Accept",
-}));
+  }));
 
 //cloudinary setUp
 const cloudinary = cloudinaryModule.v2;
